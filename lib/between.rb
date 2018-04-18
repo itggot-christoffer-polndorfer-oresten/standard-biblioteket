6ec1d542a3078b51ec9de4900082883ef1af7f6f
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2018-04-18 13:46:51 +0200

    between strict

--- lib/between.rb
+++ lib/between.rb
@@ -1,5 +1,5 @@
 def between(num_between, num2, num3)
-    if num_between < num3 && num_between > num2
+    if num_between =< num3 && num_between >= num2
         return true 
     else 
         false
---
+++ lib/between_strict.rb
@@ -0,0 +1,9 @@
+def between(num_between, num2, num3)
+    if num_between < num3 && num_between > num2
+        return true 
+    else 
+        return false
+    end 
+end   
+
+p between(1, 1, 5)
