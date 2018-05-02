15bbcdd105ea3dace7cacb6023b069458c7c4d63
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2018-05-02 13:16:05 +0200

    last_of

---
+++ lib/last_of.rb
@@ -0,0 +1,11 @@
+# Public: takes an array as input and returns the last  element in that array.
+#
+# arr - the input array
+#
+# last_of([1, 3, 4])
+# => 4
+#   
+# Returns the last element of the given input array
+def last_of(arr)
+    return arr[arr.length - 1]
+end 
