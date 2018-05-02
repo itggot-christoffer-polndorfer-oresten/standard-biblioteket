6c985ca3c31446d2755d1fa07add46c95eb9951d
itggot-christoffer-polndorfer-oresten <christoffer.polndorferoresten@itggot.se>
2018-05-02 13:11:56 +0200

    first_of

---
+++ lib/first_of.rb
@@ -0,0 +1,11 @@
+# Public: takes an array as input and returns the first element in that array.
+#
+# arr - the input array
+#
+# first_of([1, 3, 5])
+# => 1
+#
+# Returns the first element of the given input array
+def first_of(arr)
+    return arr[0]
+end 
